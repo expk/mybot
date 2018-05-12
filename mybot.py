13e709fc8fcb179739203adf4805826a684adc06
expk <expk@yandex.ru>
2018-05-12 23:10:21 +0400

    first commit

--- mybot.py
+++ mybot.py
@@ -1,4 +1,16 @@
-print("sfsdf")
+import telebot
 
 
+access_token = '596275228:AAGyWbN1Nvs0BaMZFgf1ktNYjjh9iKyRRmE'
+# Создание бота с указанным токеном доступа
+bot = telebot.TeleBot(access_token)
 
+
+# Бот будет отвечать только на текстовые сообщения
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
